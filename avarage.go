5075aac5f87d8ade7a65344f3134c667f4b45413
Shem Ogumbe <shemogumbe@microsoft.com>
2023-02-15 11:35:46 +0300

    conditionals with logical operators for if else

--- avarage.go
+++ avarage.go
@@ -11,11 +11,19 @@ func main() {
 	x = 51
 	y = 41
 
-	if x > y {
-		fmt.Printf("X is greater than Y")
+	if x > y && x < 51 {
+		fmt.Printf("X is greater  but within range")
 
 	} else {
 		fmt.Printf("Y is greater than X")
 
+	}
+
+	a := 11.0
+	b := 20.0
+
+	if frac := a / b; frac > 0.5 {
+		// read as fraction is a/b and frac is greater than 0.5
+		fmt.Println("A is more than half of b")
 	}
 }
