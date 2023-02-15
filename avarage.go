75f69ae0e079fbb5d3e9b8f40e7a77d19ca1fa69
Shem Ogumbe <shemogumbe@microsoft.com>
2023-02-15 11:31:56 +0300

    added conditional if

--- avarage.go
+++ avarage.go
@@ -5,15 +5,17 @@ import (
 )
 
 func main() {
-	var x float64
-	var y float64
+	var x int
+	var y int
 
-	x = 90.001
-	y = 80.04
+	x = 51
+	y = 41
 
-	fmt.Printf("x=%v, type of %T\n", x, x)
-	fmt.Printf("y=%v. type of %T\n", y, y)
+	if x > y {
+		fmt.Printf("X is greater than Y")
 
-	mean := (x + y) / 2
-	fmt.Printf("result: %v, type of %T\n", mean, mean)
+	} else {
+		fmt.Printf("Y is greater than X")
+
+	}
 }
