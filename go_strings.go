22d8a53259bc857e340b8c4399773a3392d95700
Shem Ogumbe <shemogumbe@microsoft.com>
2023-02-15 12:22:48 +0300

    strings in go -basics

---
+++ go_strings.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	book := "The color of magic"
+
+	fmt.Println(book)
+
+	fmt.Println(len(book))
+
+	fmt.Println("book[0] = %v (Type %T)\n", book[0], book[0])
+
+	end := book[4:]
+	fmt.Println("Ommited first -", end)
+
+	mid := book[4:10]
+	fmt.Println("Omited first and last -", mid)
+
+	start := book[:4]
+	fmt.Println("Ommited last -", start)
+}
