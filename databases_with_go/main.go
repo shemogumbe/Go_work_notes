d46f918813ec1fe2cde3f94dce891ad8ea9a7fff
Shem Ogumbe <shemogumbe@microsoft.com>
2023-03-06 15:55:36 +0300

    added route handlers

--- databases_with_go/main.go
+++ databases_with_go/main.go
@@ -10,10 +10,32 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/", indexHandler)     // Index handler
+	app.Post("/", postHandler)     // Post Handler
+	app.Put("/update", putHandler) // Put Handlerr
+	app.Delete("/delete", deleteHandler)
+
 	port := os.Getenv("PORT")
 	fmt.Println(port)
 	if port == "" {
 		port = "3000"
 	}
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
+
+func indexHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func postHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
 }
